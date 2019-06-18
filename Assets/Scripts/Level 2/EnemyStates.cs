188b3d35a040816c01219859ed6fc85c378bb8f1
Natasha Shabir <1016761@student.sae.edu.au>
2019-06-18 17:01:31 +1000

    Started Level 2 Grey Boxing, Editing Testing Level
    Used pro builder to grey box level 2.
    
    In testing level created a nav mesh script to find best way to set up enemy AI movement.

---
+++ Assets/Scripts/Level 2/EnemyStates.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyStates : MonoBehaviour
+{
+    public enum EnemyState
+    {
+        Patrolling,
+        Dead
+    }
+
+    EnemyState myState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myState = EnemyState.Dead;
+        if (Random.Range(0, 100) > 50)
+        {
+            myState = EnemyState.Patrolling;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("z"))
+        {
+            if(myState == EnemyState.Dead)
+            {
+                myState = EnemyState.Patrolling;
+            }
+            else
+            {
+                myState = EnemyState.Dead;
+            }
+
+
+        }
+
+
+        switch (myState) {
+
+            case EnemyState.Patrolling:
+                this.GetComponent<NavMeshAgent>().SetDestination(new Vector3(0, 0, 0));
+                this.GetComponent<MeshRenderer>().material.color = Color.green;
+                break;
+            case EnemyState.Dead:
+
+
+
+                this.GetComponent<MeshRenderer>().material.color = Color.red;
+                this.GetComponent<NavMeshAgent>().SetDestination(this.transform.position);
+
+
+
+
+
+
+
+
+                break;
+        }
+    }
+
+
+
+
+}
