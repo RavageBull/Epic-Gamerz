0de862132b50835f5354b90425e03c7762e27671
ElliotGordeno <1015769@student.sae.edu.au>
2019-06-25 10:03:09 +1000

    Character Controller
    Added a basic WASD character controller script that uses directions (No rotation tank stuff)

---
+++ Assets/Scripts/Generic/MoveScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed = 18;
+    private Rigidbody rig;
+    
+    void Start ()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    void Update ()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+
+        rig.MovePosition(transform.position + movement);
+    }
+}
