b7027a52bddbfe886ff2e6275bb2e757dcf6ad2b
Jake <1012334@student.sae.edu.au>
2019-06-27 18:19:35 +1000

    Shooting Script (WIP)
    the script is there but it is set to only text.

--- Assets/Scripts/Generic/Players.cs
+++ Assets/Scripts/Generic/Players.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Players : MonoBehaviour
 {
+    /*
+      public GameObject followProjectile;
+    public GameObject Projectile;
+    public Transform SpawnLoc;
+    */
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,25 @@ public class Players : MonoBehaviour
     {
         
     }
+
+    /*private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //get our child shooting location
+            //SpawnLoc = transform.Find("ShootLoc").gameObject.transform;
+            //spawn bullet prefab
+            Instantiate(Projectile, SpawnLoc.position, SpawnLoc.rotation);
+
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out hit, 100f)) ;
+        }
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                Instantiate(followProjectile, SpawnLoc.position, SpawnLoc.rotation);
+            }
+        }
+    }*/
 }
---
+++ Assets/Scripts/PlayerAbilities/BaseBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBullet : MonoBehaviour
+{/*
+    public float speed = 15;
+    public float enemyDamage = 25f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Moves Fowards based on speed
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //if not player or projectile
+        if (other.tag != "Player" && other.tag != "Projectile")
+        {
+            if (other.tag == "Enemy")
+
+            {
+                RaycastHit hit;
+                Ray ray = new Ray(transform.position, transform.forward);
+                if (Physics.Raycast(ray, out hit, 100f)) ;
+
+                hit.transform.GetComponent<HealthScript>().RemoveHealth(enemyDamage);
+                //reduce enemy health
+
+                Destroy(this.gameObject);
+                //destroy this bullet
+            }
+        }
+    }
+}
+
