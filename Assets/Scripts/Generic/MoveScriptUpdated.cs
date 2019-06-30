6683819bac3ab0153373cbb3790b9a594f09ac23
Elliot <elliot.gordeno@yahoo.com>
2019-06-30 12:54:22 +1000

    Movement Script Adjustments
    Added 
    Support for fire button mapped to A 
    Haigen comments on script 
    
    Also put MoveScriptUpdated on player2

--- Assets/Scripts/Generic/MoveScriptUpdated.cs
+++ Assets/Scripts/Generic/MoveScriptUpdated.cs
@@ -18,17 +18,65 @@ public class MoveScriptUpdated : MonoBehaviour
 
     void Update()
     {
-        float hAxis = Input.GetAxis("Horizontal");
-        float vAxis = Input.GetAxis("Vertical");
-        float HCAxis = Input.GetAxis("HorizontalController");
-
+       float hAxis = Input.GetAxis("Horizontal");
+       float vAxis = Input.GetAxis("Vertical");
+       float HCAxis = Input.GetAxis("HorizontalController");
+       //float HCVAxis = Input.GetAxis("VerticalController");
+        
+        
         Vector3 movement = new Vector3(hAxis, 0, vAxis) * agent.speed * Time.deltaTime;
 
-        rig.MovePosition(transform.position + movement);
+       rig.MovePosition(transform.position + movement);
 
-        transform.Rotate(0,HCAxis * rotationSpeed * Time.deltaTime, 0);
+       transform.Rotate(0,HCAxis * rotationSpeed * Time.deltaTime, 0);
 
-        Debug.Log(HCAxis);
+       Debug.Log(HCAxis);
 
+
+      
+
+
+
+
+
+
+
+
+        if (Input.GetButton("Fire1"))
+        {
+            print("Test");
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            print("This is controller 2");
+        }
     }
+
+
+
+
+
+
+
+    /// float hAxis = Input.GetAxis("Horizontal");
+    /// float vAxis = Input.GetAxis("Vertical");
+    /// float HCAxis = Input.GetAxis("HorizontalController");
+    /// Add another input for rotating your right stick
+    /// float HCVAxis = Input.GetAxis("VerticalController");
+
+    /// Rework this to be more in line with what you need:
+    /// Vector3 moveDirection = new Vector3(hAxis, 0, vAxis);
+    /// Vector3 movement = new Vector3(hAxis, 0, vAxis) * agent.speed * Time.deltaTime;
+    /// Add a lookDir variable to use
+    /// Vector3 lookDirection = new Vector3(HCAxis, 0, HCVAxis);
+
+    /// transform.rotation = Quaternion.Euler(lookDirection);
+		/// Move after rotation instead of before, this is causing your issues
+		/// rig.MovePosition(moveDirection); 
+		/// Can use moveDirection * speed instead;
+        /// rig.MovePosition(transform.position + movement);
+
+        /// transform.Rotate(0,HCAxis* rotationSpeed * Time.deltaTime, 0);
+
+        /// Debug.Log(HCAxis);
 }
